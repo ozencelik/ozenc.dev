dbd0082a7fee9c9f6503b9ade78a07de7cd48b6b
Ozenc Celik <ozenc.celik@hotmail.com>
2022-12-10 21:39:15 +0300

    Styling & Cleaning home page
    * Global css file added to style all pages at the same time
    * Redundant code deleted

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -6,21 +6,11 @@ import Link from "next/link";
 
 export default function FirstPost() {
     return (
-        <>
+        <Layout>
             <Head>
                 <title>First Post</title>
             </Head>
-
             <h1>First Post</h1>
-            <h2>
-                <Link href="/">← Back to home</Link>
-                <Image
-                    src="/images/profile.png"
-                    width={50}
-                    height={50}
-                    alt="Özenç Çelik"
-                />
-            </h2>
-        </>
+        </Layout>
     );
 }
